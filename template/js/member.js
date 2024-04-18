2034a5fde41e62f931c615fc83f3995330723997
RND-213 <rnd-213@excell-tw.local>
2024-04-18 17:01:22 +0800

    '20240418施作會員中心'

--- template/js/member.js
+++ template/js/member.js
@@ -1,5 +1,5 @@
+// 登入頁面
 $(function () {
-	// 登入頁面
 	if ($('.login-box').length) {
 		$('.login-form ').show();
 		$('.forgot-password-form').hide();
@@ -15,5 +15,36 @@ $(function () {
 			$('.login-form ').fadeIn(500);
 			$('.forgot-password-form').hide();
 		});
+	}
+});
+
+// 註冊畫面
+$(function () {
+	if ($('.twzipcode-member').length) {
+		$('.twzipcode-member').twzipcode({
+			zipcodeIntoDistrict: false, // 郵遞區號自動顯示在區別選單中
+			county: {
+				value: '請選擇縣市*',
+				label: '郵遞區號',
+			},
+
+			district: {},
+			zipcode: {
+				css: 'townNum',
+			},
+			countyName: 'city', // 自訂城市 select 標籤的 name 值
+			districtName: 'town', // 自訂區別 select 標籤的 name 值
+		});
+		$(`.twzipcode-member input[name="zipcode"]`)
+			.addClass('form-input')
+			.attr('required', true)
+			.prop('required', true)
+			.attr('placeholder', '郵遞區號');
+		$(
+			`.twzipcode-member select[name="county"],.twzipcode-member select[name="district"]`
+		)
+			.addClass('form-select')
+			.attr('required', true)
+			.prop('required', true);
 	}
 });
