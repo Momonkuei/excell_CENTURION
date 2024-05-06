50f3956390eb9331859037f32438d3f448042ebe
RND-213 <rnd-213@excell-tw.local>
2024-05-06 16:58:01 +0800

    '20240506product_category頁面更新'

--- template/js/product.js
+++ template/js/product.js
@@ -19,8 +19,38 @@ $(function () {
 
 // 產品大類 product_category
 $(function () {
-	if ($('.product-category-banner').length) {
-		const category_swiper = new Swiper('.category-swiper');
+	if ($('.product-category-banner-box').length) {
+		const categoryBanner_swiper = new Swiper('.categoryBanner-swiper');
+	}
+
+	if ($('.sectionBlock-category-carousel').length) {
+		const category_swiper = new Swiper('.category-carousel-swiper', {
+			loop: true,
+			slidesPerView: 1,
+			// spaceBetween: 30,
+			// centeredSlides: true,
+			navigation: {
+				nextEl: '.swiper-button-next-custom',
+				prevEl: '.swiper-button-prev-custom',
+			},
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+			breakpoints: {
+				576: {
+					slidesPerView: 2,
+				},
+
+				992: {
+					slidesPerView: 'auto',
+				},
+
+				1200: {
+					slidesPerView: 5,
+				},
+			},
+		});
 	}
 });
 
