2743555cfa497355805879954ae8ba89aa0c3aec
RND-213 <rnd-213@excell-tw.local>
2024-05-14 16:43:51 +0800

    '20240514修正完成'

---
+++ 線上備份/20240514/JS/03/cart.js
@@ -0,0 +1,148 @@
+// cart
+$(function () {
+	if ($('.cart-item').length) {
+		$('.cart-item').each(function () {
+			let $item = $(this);
+			let product_QTY = parseInt(
+				$item.find('.QTY-selector-currentNum').val()
+			);
+			$item.find('.QTY-reduce').click(function () {
+				product_QTY--;
+				if (product_QTY <= 0) {
+					return (product_QTY = 1);
+				}
+				$item.find('.QTY-selector-currentNum').val(product_QTY);
+			});
+
+			$item.find('.QTY-plus').click(function () {
+				product_QTY++;
+				$item.find('.QTY-selector-currentNum').val(product_QTY);
+			});
+		});
+	}
+});
+
+// cart2
+
+$(function () {
+	// 添加必填以及placeholder
+	const addtwzipcode = function (num) {
+		$(`.twzipcode${num} input[name="zipcode"]`)
+			.addClass('form-input')
+			.prop('required', true)
+			.prop('readonly', true)
+			.attr('placeholder', '郵遞區號');
+		$(
+			`.twzipcode${num} select[name="county"],.twzipcode${num} select[name="district"]`
+		)
+			.addClass('form-select')
+			.prop('required', true);
+	};
+
+	// 聯絡資訊
+
+	if ($('.twzipcode1').length) {
+		$('.twzipcode1').twzipcode({
+			zipcodeIntoDistrict: false, // 郵遞區號自動顯示在區別選單中
+			county: {
+				value: '請選擇縣市*',
+				label: '郵遞區號',
+			},
+
+			district: {},
+			zipcode: {
+				css: 'townNum',
+			},
+			countyName: 'city', // 自訂城市 select 標籤的 name 值
+			districtName: 'town', // 自訂區別 select 標籤的 name 值
+		});
+		addtwzipcode(1);
+	}
+
+	// 運送
+	if ($('.twzipcode2').length) {
+		$('.twzipcode2').twzipcode({
+			zipcodeIntoDistrict: false, // 郵遞區號自動顯示在區別選單中
+			county: {
+				value: '請選擇縣市*',
+				label: '郵遞區號',
+			},
+
+			district: {},
+			zipcode: {
+				css: 'townNum',
+			},
+			countyName: 'city', // 自訂城市 select 標籤的 name 值
+			districtName: 'town', // 自訂區別 select 標籤的 name 值
+		});
+		addtwzipcode(2);
+	}
+
+	// 發票類型
+	if ($('#invoice_type').length) {
+		$('#invoice_type').on('change', function () {
+			$(
+				'.invoice-area, .company-account-invoice-area,.mobileBarcode-area'
+			).hide();
+
+			// 去除 required
+			$('.company-account-invoice-area input').each(function () {
+				$(this).prop('required', false);
+			});
+
+			$('.mobileBarcode-area input').each(function () {
+				$(this).prop('required', false);
+			});
+
+			const $invoiceType = $('#invoice_type option:selected').val();
+			$(`.${$invoiceType}`).show();
+
+			// 添加 required
+			if ($invoiceType === 'company-account-invoice-area') {
+				$(`.${$invoiceType} input`).each(function () {
+					$(this).prop('required', true);
+				});
+			}
+
+			// 個人發票 載具類型歸位
+			if ($invoiceType === 'invoice-area') {
+				$(`.${$invoiceType} select`).val(0);
+			}
+		});
+	}
+
+	// 載具類型
+	if ($('#vehicle_type').length) {
+		$('#vehicle_type').on('change', function () {
+			$('.mobileBarcode-area').hide();
+			// 去除 required
+
+			$('.mobileBarcode-area input').each(function () {
+				$(this).prop('required', false);
+			});
+
+			const $vehicleType = $('#vehicle_type option:selected').val();
+			$(`.${$vehicleType}`).show();
+
+			// 添加 required
+			if ($vehicleType === 'mobileBarcode-area') {
+				$('.mobileBarcode-area input').prop('required', true);
+			}
+		});
+	}
+
+	// 手機板收合訂單內容
+	if ($('.order-content-btn').length) {
+		$('.order-content-btn').click(function () {
+			if ($(this).hasClass('active')) {
+				$('.right-box-area').slideUp();
+				$('.order-content-btn').removeClass('active');
+				$(this).find('.txt').text('顯示訂單內容');
+			} else {
+				$('.right-box-area').slideDown();
+				$('.order-content-btn').addClass('active');
+				$(this).find('.txt').text('隱藏訂單內容');
+			}
+		});
+	}
+});
---
+++ 線上備份/20240514/JS/03/product.js
@@ -0,0 +1,182 @@
+// 側邊攔
+$(function () {
+	if ($('.product-sidebar').length) {
+		$('.product-sidebar .product-sidebar-item .second-menu-btn').click(
+			function () {
+				$(this).parent('.product-sidebar-item').toggleClass('open');
+			}
+		);
+
+		// 找尋是否有active 並且打開 product-sidebar-item
+		var activeLink = $('.product-sidebar-item a.active');
+
+		if (activeLink.length > 0) {
+			activeLink.each(function () {
+				$(this).closest('.product-sidebar-item').addClass('open');
+			});
+		}
+	}
+});
+
+// 產品大類 product_category
+$(function () {
+	if ($('.product-category-banner-box').length) {
+		const categoryBanner_swiper = new Swiper('.categoryBanner-swiper');
+	}
+
+	if ($('.sectionBlock-category-carousel').length) {
+		const category_swiper = new Swiper('.category-carousel-swiper', {
+			loop: true,
+			slidesPerView: 1,
+			autoplay: {
+				delay: 2500,
+			},
+			navigation: {
+				nextEl: '.swiper-button-next-custom',
+				prevEl: '.swiper-button-prev-custom',
+			},
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+			breakpoints: {
+				576: {
+					slidesPerView: 2,
+				},
+				992: {
+					slidesPerView: 'auto',
+				},
+				1200: {
+					slidesPerView: 4,
+				},
+				1500: {
+					loop: false,
+					slidesPerView: 5,
+				},
+			},
+		});
+	}
+});
+
+// 產品列表分頁
+$(function () {
+	if ($('.pagination-box .pagination-page').length) {
+		$('.pagination-box .pagination-page .pageLink').click(function (idx) {
+			$('.pagination-box .pagination-page .pageLink').each(function () {
+				$(this).removeClass('active');
+			});
+			$(this).addClass('active');
+		});
+	}
+});
+
+// 產品介紹滑塊
+$(function () {
+	if ($('.product-gallery').length) {
+		// 縮圖
+		const product_thumbs = new Swiper('.gallery-thumbs', {
+			// slidesPerView: 'auto',
+			slidesPerView: 6,
+			spaceBetween: 10,
+			loop: true,
+			slideToClickedSlide: true,
+			centeredSlides: true,
+			freeMode: true,
+			watchSlidesVisibility: true,
+			watchSlidesProgress: true,
+		});
+
+		const product_slider = new Swiper('.gallery-slider', {
+			slidesPerView: 1,
+			loop: true,
+			navigation: {
+				nextEl: '.gallery-slider-next',
+				prevEl: '.gallery-slider-prev',
+			},
+			pagination: {
+				el: '.swiper-pagination',
+			},
+			thumbs: {
+				swiper: product_thumbs,
+				autoScrollOffset: 1,
+			},
+		});
+
+		// 與swiper 的按鈕綁定
+		if ($('.product-gallery-btn-prev').length) {
+			$('.product-gallery-btn-prev').click(() =>
+				$('.gallery-slider-prev').click()
+			);
+		}
+
+		if ($('.product-gallery-btn-next').length) {
+			$('.product-gallery-btn-next').click(() =>
+				$('.gallery-slider-next').click()
+			);
+		}
+	}
+});
+
+// 產品內頁
+$(function () {
+	// 數量
+	if ($('.sectionblock-product-detail .product-item-quantity').length) {
+		let product_Num = parseInt(
+			$('.product-item-quantity .product-item-num').val()
+		);
+		$('.product-item-quantity .quantity-reduce').click(function () {
+			product_Num--;
+			if (product_Num <= 0) {
+				return (product_Num = 1);
+			}
+			$('.product-item-quantity .product-item-num').val(product_Num);
+		});
+
+		$('.product-item-quantity .quantity-plus').click(function () {
+			product_Num++;
+			$('.product-item-quantity .product-item-num').val(product_Num);
+		});
+	}
+
+	//  產品介紹
+	const $body = window.opera
+		? document.compatMode == 'CSS1Compat'
+			? $('html')
+			: $('body')
+		: $('html,body');
+	if ($('.product-item-collapsible-lists').length) {
+		$('.product-item-collapsible-btn').click(function (idx) {
+			const $activecollapsible = $(this).parent(
+				'.product-item-collapsible'
+			);
+			if ($activecollapsible.hasClass('active')) {
+				$activecollapsible.removeClass('active');
+			} else {
+				$('.product-item-collapsible').each(function () {
+					$(this).removeClass('active');
+				});
+				$activecollapsible.addClass('active');
+
+				// 抓取動畫時間
+				var transitionDuration = $(
+					'.product-item-collapsible-inner'
+				).css('transition-duration');
+
+				const durationInSeconds = parseFloat(transitionDuration) + 0.1;
+
+				// 抓取header高度
+				const hdTop = $('header').outerHeight();
+
+				setTimeout(function () {
+					const target_top = $activecollapsible.offset().top;
+					$body.animate(
+						{
+							scrollTop: target_top - hdTop - 10,
+						},
+						200
+					);
+				}, durationInSeconds * 1000);
+			}
+		});
+	}
+});
